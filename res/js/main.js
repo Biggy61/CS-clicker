5f0852afc547b33f0dfc0c0257eec21acad543c8
Biggy61 <zdanskystepanek@gmail.com>
2023-09-27 10:29:05 +0200

    added upgrade button

--- res/js/main.js
+++ res/js/main.js
@@ -1,8 +1,8 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
-
+const upgradeClickButton = document.getElementById("upgradeClickButton")
 let numberOfCookies = 0;
-
+let clickIncrease = 1;
 // {} - scope
 
 cookie.onclick = () => {
@@ -10,7 +10,19 @@ cookie.onclick = () => {
     //numberOfCookies = numberOfCookies + 1;
     //numberOfCookies += 1;
     // ++ inkrement
-    numberOfCookies++;
+    // => arrow funkce
+    numberOfCookies += clickIncrease;
     console.log(numberOfCookies);
     counter.innerHTML = "RP: " + numberOfCookies;
+}
+
+upgradeClickButton.onclick = () => {
+    if (numberOfCookies >= 50){
+        //odectu susenky
+        numberOfCookies -= 50;
+        counter.innerHTML = "RP: " + numberOfCookies;
+        
+        //zvednu pocet kliknuti
+        clickIncrease ++
+    }
 }
